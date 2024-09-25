d90826b8fc12b6bb208a6f65736fbb638357a35a
Chaerry22 <dchae22@gmail.com>
2024-09-25 15:25:41 -0400

    :wq
    :wq
    i
    
    d
    d
    d
    
    i

---
+++ components/CreateEvent.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function CreateEventScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Create your event here.</Text>
+    </View>
+  );
+}
