bb41a8e3155f68b1318fdac700246f584d426a74
Chaerry22 <dchae22@gmail.com>
2024-11-22 14:26:34 -0500

    lab11

--- app/communitiesNavigation.tsx
+++ app/communitiesNavigation.tsx
@@ -3,4 +3,5 @@
 export type RootStackParamList = {
   Communities: undefined; // No params for Communities screen
   CommunityDetails: { communityId: string }; // Expect communityId as a string
+  
 };
