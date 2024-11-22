5497e3ae3949fa9d562230920a138b6eae0ab227
soyHaDong <hadongpark10@gmail.com>
2024-11-22 01:15:33 -0500

    save envents

--- app/(tabs)/savedEvents.tsx
+++ app/(tabs)/savedEvents.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from '@/styles/globalStyles';
+import React from "react";
+import styles from "@/styles/globalStyles";
+import {
+  View,
+  Text,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  KeyboardAvoidingView,
+} from "react-native";
+import { SavedEventsProvider } from "../../app/saveEventsContext";
 
 export default function savedEvents() {
   return (
     <View style={styles.container}>
-        <Text>Saved Events</Text>
+      {savedEvents.length === 0 ? (
+        <Text>No saved events yet.</Text>
+      ) : (
+        <FlatList
+          // data={savedEvents}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.cardText}>{item.name}</Text>
+              <Text style={styles.cardText}>{item.date}</Text>
+              <Text style={styles.cardDescription}>{item.description}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 }
